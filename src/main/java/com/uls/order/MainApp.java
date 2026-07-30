ff1d483a083342b1fc3a47cb894511db1b41edec
Saeed Zare <saeed1381@gmail.com>
2026-07-30 21:21:03 +0330

    feat(order-integration): add observability interceptor

--- src/main/java/com/uls/order/MainApp.java
+++ src/main/java/com/uls/order/MainApp.java
@@ -1,13 +1,21 @@
 package com.uls.order;
 
+import org.apache.camel.CamelConfiguration;
+import org.apache.camel.CamelContext;
 import org.apache.camel.main.Main;
 
 public class MainApp {
     public static void main(String[] args) throws Exception {
         Main main = new Main();
-        
+
         main.configure().addRoutesBuilder(new OrderProcessorRoute());
         main.configure().addRoutesBuilder(new StatusReportRoute());
+        main.configure().addConfiguration(new CamelConfiguration() {
+            @Override
+            public void configure(CamelContext camelContext) {
+                camelContext.getCamelContextExtension().addInterceptStrategy(new ObservabilityInterceptStrategy());
+            }
+        });
 
         main.run(args);
     }
